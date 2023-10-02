a4ec2f128c21abbc00c0a8eefdf7299dd957f25b
PradyumnaCh <35379042+PradyumnaCh@users.noreply.github.com>
2023-10-02 02:48:07 -0400

    Including attention mask in the outputs
    - some code clean up
    - main method to test

--- encoder.py
+++ encoder.py
@@ -1,10 +1,11 @@
-import torch
-from transformers import (
-    AutoModel,
-    AutoTokenizer,
-)
+from typing import List, Dict
+
 from enum import Enum
 from dataclasses import dataclass
+from typing import Dict, List
+
+import torch
+from transformers import AutoModel, AutoTokenizer
 
 
 @dataclass
@@ -33,28 +34,26 @@ class LLaMaEncoder:
 
 class EncoderType(Enum):
     """Type of encoder to use"""
+
     LongFormer = LongformerEncoder
     LongT5 = LongT5Encoder
     LLaMa = LLaMaEncoder
 
 
 class Encoder:
-    def __init__(
-        self, encoder: EncoderType, chunk_size: int = 4096, pooled_outputs: bool = False
-    ):
+    def __init__(self, encoder: EncoderType, chunk_size: int = 4096):
         """
         :param encoder: Type of encoder to use
         :param chunk_size: Size of chunks to split the document into
         :param pooled_outputs: Whether to return pooled outputs
         """
         self.chunk_size = chunk_size
-        self.pooled_outputs = pooled_outputs
 
         if encoder not in EncoderType:
             raise ValueError(f"Encoder must be one of {list(EncoderType)}")
-        
+
         self.encoder_type = encoder
-        self.encoder = EncoderType(encoder)
+        self.encoder = self.encoder_type.value()
         self.model = AutoModel.from_pretrained(self.encoder.model_id)
         self.tokenizer = AutoTokenizer.from_pretrained(self.encoder.tokenizer_id)
 
@@ -65,29 +64,62 @@ class Encoder:
 
         self.tokenizer.model_max_length = self.model.config.max_position_embeddings
 
-    def encode(self, text: str) -> torch.Tensor:
+    def encode(self, text: List[str]) -> Dict[str, torch.Tensor]:
         """
         Encode a long document using an encoder
         :param text: Document to encode
         :return: Encoded document
         """
-        input_ids = self.tokenizer.encode(text, return_tensors="pt").unsqueeze(0)
+        tokenized_text = self.tokenizer(
+            text, return_tensors="pt", return_attention_mask=True, padding=True
+        )
+        input_ids = tokenized_text.input_ids
+        attention_mask = tokenized_text.attention_mask
 
         # encode each chunk
         outputs = []
         for i in range(0, input_ids.shape[1], self.chunk_size):
             chunk_input_ids = input_ids[:, i : i + self.chunk_size]
+            chunk_attention_mask = attention_mask[:, i : i + self.chunk_size]
             if self.encoder_type == EncoderType.LongFormer:
-                global_attention_mask = self.encoder.get_global_attention_mask(chunk_input_ids)
+                global_attention_mask = self.encoder.get_global_attention_mask(
+                    chunk_input_ids
+                )
                 output = self.model(
-                    input_ids=chunk_input_ids, global_attention_mask=global_attention_mask
+                    input_ids=chunk_input_ids,
+                    attention_mask=chunk_attention_mask,
+                    global_attention_mask=global_attention_mask,
                 )
             else:
                 output = self.model(input_ids=chunk_input_ids)
             outputs.append(output)
 
         # concatenate outputs
-        if self.pooled_outputs:
-            return torch.cat([output.pooler_output for output in outputs], dim=1)
-        else:
-            return torch.cat([output.last_hidden_state for output in outputs], dim=1)
+        pooler_outputs = torch.cat([output.pooler_output for output in outputs], dim=1)
+        last_hidden_outputs = torch.cat(
+            [output.last_hidden_state for output in outputs], dim=1
+        )
+
+        encoder_outputs = {
+            "pooler_output": pooler_outputs,
+            "last_hidden_state": last_hidden_outputs,
+            "attention_mask": attention_mask,
+        }
+
+        return encoder_outputs
+
+
+if __name__ == "__main__":
+    # test encoder on a long document
+    test_text = [
+        " ".join(["This is a test sentence." for _ in range(1000)]),
+        " ".join(
+            [
+                "This is another test sentence with different length."
+                for _ in range(1000)
+            ]
+        ),
+    ]
+    encoder = Encoder(EncoderType.LongFormer)
+    test_outputs = encoder.encode(test_text)
+    print(test_outputs.shape)
