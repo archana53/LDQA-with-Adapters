e735d31ec377bb896eb052b24840850b1563de3b
PradyumnaCh <35379042+PradyumnaCh@users.noreply.github.com>
2023-10-03 02:39:15 -0400

    Adding LDQA Model for HF Trainer

--- model.py
+++ model.py
@@ -1,76 +1,64 @@
-
-import torch
-from torch import nn
-import torch.nn.functional as F
-from transformers import LongformerModel, RobertaTokenizer
-from transformers import LongformerConfig
-from torchsummary import summary
+import torch.nn as nn
+from transformers import PreTrainedModel, PretrainedConfig
 
 
-def add_cross_attention_modules(model):
-    print(model)
+class LDQAModelConfig(PretrainedConfig):
+    model_type = "ldqa"
+
+    def __init__(self, model_type="longformer", **kwargs):
+        super().__init__(**kwargs)
+        if model_type != "longformer":
+            raise ValueError("Only longformer is supported")
+        self.model_type = model_type
 
 
-def pad_to_window_size(input_ids: torch.Tensor, attention_mask: torch.Tensor,
-                       one_sided_window_size: int, pad_token_id: int):
-    '''A helper function to pad tokens and mask to work with the sliding_chunks implementation of Longformer selfattention.
-    Input:
-        input_ids = torch.Tensor(bsz x seqlen): ids of wordpieces
-        attention_mask = torch.Tensor(bsz x seqlen): attention mask
-        one_sided_window_size = int: window size on one side of each token
-        pad_token_id = int: tokenizer.pad_token_id
-    Returns
-        (input_ids, attention_mask) padded to length divisible by 2 * one_sided_window_size
-    '''
-    w = int(2 * one_sided_window_size)
-    seqlen = input_ids.size(1)
-    padding_len = (w - seqlen % w) % w
-    input_ids = F.pad(input_ids, (0, padding_len), value=pad_token_id)
-    attention_mask = F.pad(attention_mask, (0, padding_len), value=False)  # no attention on the padding tokens
-    return input_ids, attention_mask
+class LDQAModel(PreTrainedModel):
+    config_class = LDQAModelConfig
 
-class BaseModel(object):
-    def __init__(self,**kwargs):
-        super(BaseModel, self).__init__(**kwargs)
+    def __init__(self, config):
+        super().__init__(config)
+        if config.model_type == "longformer":
+            # TODO: WIP. Functions are not implemented yet.
+            self.base_lm = get_longformer_with_cross_attention()
+            self.encoder = get_longformer_encoder()
+        self.loss_fct = nn.CrossEntropyLoss()
 
-class LongFormer(BaseModel):
-    def __init__(self,**kwargs):
-        super(LongFormer, self).__init__(**kwargs)
-        self.hf_model_uuid = "allenai/longformer-base-4096"
-        self.model = LongformerModel.from_pretrained(self.hf_model_uuid)
+    def forward(
+        self,
+        query_ids,
+        document_ids,
+        query_attention_mask=None,
+        document_attention_mask=None,
+        global_attention_mask=None,
+        labels=None,
+    ):
+        """Performs a forward pass through the model. Returns loss and logits if labels are provided else returns logits only.
+        :param query_ids: torch.LongTensor of shape [batch_size, query_length]
+        :param document_ids: torch.LongTensor of shape [batch_size, document_length]
+        :param query_attention_mask: torch.LongTensor of shape [batch_size, query_length]
+        :param document_attention_mask: torch.LongTensor of shape [batch_size, document_length]
+        :param global_attention_mask: torch.LongTensor of shape [batch_size, document_length]
+        :param labels: torch.LongTensor of shape [batch_size, labels_length]
+        :return: loss, logits
+        """
+        # get encoded document
+        document_outputs = self.encoder(
+            document_ids,
+            attention_mask=document_attention_mask,
+            global_attention_mask=global_attention_mask,
+        )
 
-    def get_model(self):
-        return self.model
-    def forward(self,x):
-        self.model(x)
+        # pass encoded document and query to base-lm
+        base_lm_outputs = self.base_lm(
+            query_ids,
+            attention_mask=query_attention_mask,
+            cross_modality_inputs=document_outputs.last_hidden_state,
+            cross_modality_attention_mask=document_outputs.attention_mask,
+        )
 
-
-if __name__ == '__main__':
-    config = LongformerConfig()
-    model = LongformerModel(config, cross_modality = True)
-    add_cross_attention_modules(model)
-
-    """
-    model = model.model
-    tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
-    tokenizer.model_max_length = model.config.max_position_embeddings
-
-    SAMPLE_TEXT = ' '.join(['Hello world! '] * 1000)  # long input document
-
-    input_ids = torch.tensor(tokenizer.encode(SAMPLE_TEXT)).unsqueeze(0)  # batch of size 1
-
-    # TVM code doesn't work on CPU. Uncomment this if `config.attention_mode = 'tvm'`
-    # model = model.cuda(); input_ids = input_ids.cuda()
-
-    # Attention mask values -- 0: no attention, 1: local attention, 2: global attention
-    attention_mask = torch.ones(input_ids.shape, dtype=torch.long, device=input_ids.device) # initialize to local attention
-    attention_mask[:, [1, 4, 21,]] =  2  # Set global attention based on the task. For example,
-                                        # classification: the <s> token
-                                        # QA: question tokens
-
-    # padding seqlen to the nearest multiple of 512. Needed for the 'sliding_chunks' attention
-    input_ids, attention_mask = pad_to_window_size(
-            input_ids, attention_mask, config.attention_window[0], tokenizer.pad_token_id)
-
-    output = model(input_ids, attention_mask=attention_mask)[0]
-    """
+        # compute loss if labels are provided
+        if labels is not None:
+            loss = self.loss_fct(base_lm_outputs.logits, labels)
+            return loss, base_lm_outputs.logits
+        else:
+            return base_lm_outputs.logits
