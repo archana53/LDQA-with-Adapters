b9bece26453b98d5e70e81599c3d58832d2590c8
PradyumnaCh <35379042+PradyumnaCh@users.noreply.github.com>
2023-10-05 18:40:10 -0400

    Using LDQA Model for abstraction
    - repalced HF data collator with custom implementation
    - code cleanup

--- hf_train.py
+++ hf_train.py
@@ -1,22 +1,18 @@
-from dataset import MuLD_Dataset
-from torch import optim, utils
-from torch.utils.data import DataLoader
-from transformers import (
-    LongformerModel,
-    RobertaTokenizer,
-    LongformerForQuestionAnswering,
-    AutoTokenizer,
-    DataCollatorForSeq2Seq
-)
-from transformers import Trainer, TrainingArguments
-from transformers import LongformerConfig
-from transformers import get_scheduler
 import argparse
-from torch.optim import Adam
-from encoder import Encoder, EncoderType
 import itertools
 
-#breakpoint()
+from torch.optim import Adam
+from transformers import (
+    AutoTokenizer,
+    LongformerModel,
+    Trainer,
+    TrainingArguments,
+    get_scheduler,
+)
+
+from dataset import DataCollatorForLDQA, MuLD_Dataset
+from encoder import Encoder, EncoderType
+from model import LDQAModel, LDQAModelConfig
 
 
 if __name__ == "__main__":
@@ -32,55 +28,66 @@ if __name__ == "__main__":
 
     # set up base-lm and document encoder
     model_original = LongformerModel.from_pretrained("allenai/longformer-base-4096")
-    model = LongformerModel(model_original.config, cross_modality=True)
-    model.load_state_dict(model_original.state_dict(), strict = False)
+    base_lm = LongformerModel(model_original.config, cross_modality=True)
+    base_lm.load_state_dict(model_original.state_dict(), strict=False)
 
     model_tokenizer = AutoTokenizer.from_pretrained("allenai/longformer-base-4096")
     encoder = Encoder(EncoderType.LongFormer)
-    
-    muld_object = MuLD_Dataset()
-    val_dataset = muld_object.get_dataset(
-        split="validation", streaming=True
+
+    # set up LDQA model
+    model_config = LDQAModelConfig()
+    model = LDQAModel(model_config, base_lm, encoder)
+
+    muld_object = MuLD_Dataset(
+        tokenizer=model_tokenizer, split=None, streaming=True, chunk_size=4096
+    )
+    train_dataset = muld_object.dataset["train"]
+    val_dataset = muld_object.dataset["validation"]
+
+    data_collator = DataCollatorForLDQA(
+        tokenizer=model_tokenizer,
+        padding="max_length",
+        max_query_length=4096,
+        return_tensors="pt",
     )
 
-    # Tokenize your dataset using the tokenizer
-    def tokenize_function(examples):
-        return model_tokenizer(examples["input"], padding="max_length", truncation=True)
-
-    tokenized_datasets = val_dataset.map(tokenize_function, batched=True)
-
-    data_collator = DataCollatorForSeq2Seq(tokenizer=model_tokenizer, padding = 'max_length', max_length = 4096 )
-
+    total_steps = 32000
     training_args = TrainingArguments(
-    output_dir="./output",
-    num_train_epochs=3,  # Adjust based on your requirements
-    per_device_train_batch_size=8,
-    save_steps=1000,
-    save_total_limit=2,
+        output_dir="./output",
+        num_train_epochs=3,  # Adjust based on your requirements
+        per_device_train_batch_size=2,
+        save_steps=1000,
+        save_total_limit=2,
+        max_steps=total_steps,
+        remove_unused_columns=False,
     )
 
     trainable_params = []
     trainable_mod_names = []
 
-    #Only cross attention parameters trainable
-    for name,module in model.named_modules():
-        if 'cross' in name:
+    # Only cross attention parameters trainable
+    for name, module in model.named_modules():
+        if "cross" in name:
             trainable_params.append(module.parameters())
-    
-    trainable_params = (itertools.chain(*trainable_params))
+
+    trainable_params = itertools.chain(*trainable_params)
     print(trainable_params)
     optimizer = Adam(trainable_params, lr=1e-3)
 
-    total_steps = len(list(tokenized_datasets)) * training_args.num_train_epochs / 8
-    lr_scheduler = get_scheduler(name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=total_steps)
+    lr_scheduler = get_scheduler(
+        name="linear",
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=total_steps,
+    )
 
     trainer = Trainer(
-    model,
-    training_args,
-    train_dataset=val_dataset,
-    eval_dataset=val_dataset,
-    tokenizer=model_tokenizer,
+        model,
+        training_args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        tokenizer=model_tokenizer,
+        data_collator=data_collator,
     )
 
     trainer.train()
-    
