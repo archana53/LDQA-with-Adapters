dc7dbfda0f1e34e49fc41acc93819df68ac1718c
archana53 <archanakutumbaka7@gmail.com>
2023-10-21 10:26:23 -0400

    [Dataset] Minor changes

--- dataset.py
+++ dataset.py
@@ -12,25 +12,29 @@ class Dataset(object):
         raise NotImplementedError
 
 
-class MuLD_Dataset(Dataset): 
+class MuLD_Dataset(Dataset):
     def __init__(self):
         super(MuLD_Dataset, self).__init__()
         self.dataset_uuid = "ghomasHudson/muld"
 
-    def get_dataset(self, split = 'train', streaming = False) -> dict:
-        dataset = load_dataset(self.dataset_uuid, 'NarrativeQA', split = split, streaming= streaming)
+    def get_dataset(self, split="train", streaming=False) -> dict:
+        dataset = load_dataset(
+            self.dataset_uuid, "NarrativeQA", split=split, streaming=streaming
+        )
         return dataset
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     muld = MuLD_Dataset()
-    dset = muld.get_dataset(split = 'validation', streaming = True)
-    train_dset = dset.with_format('torch')
+    dset = muld.get_dataset(split="validation", streaming=True)
+    print(dset)
+    train_dset = dset.with_format("torch")
     train_loader = DataLoader(train_dset, batch_size=4)
-    print(train_loader)
+    # print(train_loader)
     for example in dset:
-        print(example.keys())
-        print(example['input'][:500])
-        print("output is ", example['output'])
-        print(example['metadata'])
-        #dict containing 'input', 'output', 'metadata'
+        # print(example.keys())
+        # print(example["input"][:500])
+        # print("output is ", example["output"])
+        # print(example["metadata"])
+        # dict containing 'input', 'output', 'metadata'
         break
--- hf_train.py
+++ hf_train.py
@@ -1,27 +1,36 @@
-from dataset import MuLD_Dataset
-from torch import optim, utils
-from torch.utils.data import DataLoader
-from transformers import (
-    LongformerModel,
-    RobertaTokenizer,
-    LongformerForQuestionAnswering,
-    AutoTokenizer,
-    DataCollatorForSeq2Seq
-)
-from transformers import Trainer, TrainingArguments
-from transformers import LongformerConfig
-from transformers import get_scheduler
 import argparse
-from torch.optim import Adam
-from encoder import Encoder, EncoderType
 import itertools
 
-#breakpoint()
+from datasets import IterableDataset
+from torch import optim, utils
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+
+from dataset import MuLD_Dataset
+from encoder import EncoderType
+from transformers import (
+    AutoTokenizer,
+    DataCollatorForSeq2Seq,
+    LongformerConfig,
+    LongformerForQuestionAnswering,
+    LongformerModel,
+    RobertaTokenizer,
+    Trainer,
+    TrainingArguments,
+    get_scheduler,
+)
+
+# breakpoint()
+
+
+def my_generator(n):
+    for i in range(n):
+        yield {"input": "sample text", "output": "filler text", "metadata": []}
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--batch_size", type=int, default=1)
     parser.add_argument(
         "--encoder_type",
         type=str,
@@ -32,16 +41,18 @@ if __name__ == "__main__":
 
     # set up base-lm and document encoder
     model_original = LongformerModel.from_pretrained("allenai/longformer-base-4096")
-    model = LongformerModel(model_original.config, cross_modality=True)
-    model.load_state_dict(model_original.state_dict(), strict = False)
+    model = LongformerModel(model_original.config, cross_attn=True)
+    model.load_state_dict(model_original.state_dict(), strict=False)
+
+    print(model.config)
 
     model_tokenizer = AutoTokenizer.from_pretrained("allenai/longformer-base-4096")
-    encoder = Encoder(EncoderType.LongFormer)
-    
+    encoder = EncoderType.LongFormer.value()
+
     muld_object = MuLD_Dataset()
-    val_dataset = muld_object.get_dataset(
-        split="validation", streaming=True
-    )
+    val_dataset = muld_object.get_dataset(split="validation", streaming=True)
+
+    sample_dataset = IterableDataset.from_generator(my_generator, gen_kwargs={"n": 1})
 
     # Tokenize your dataset using the tokenizer
     def tokenize_function(examples):
@@ -49,38 +60,47 @@ if __name__ == "__main__":
 
     tokenized_datasets = val_dataset.map(tokenize_function, batched=True)
 
-    data_collator = DataCollatorForSeq2Seq(tokenizer=model_tokenizer, padding = 'max_length', max_length = 4096 )
+    data_collator = DataCollatorForSeq2Seq(
+        tokenizer=model_tokenizer, padding="max_length", max_length=4098
+    )
 
     training_args = TrainingArguments(
-    output_dir="./output",
-    num_train_epochs=3,  # Adjust based on your requirements
-    per_device_train_batch_size=8,
-    save_steps=1000,
-    save_total_limit=2,
+        output_dir="./output",
+        num_train_epochs=3,  # Adjust based on your requirements
+        per_device_train_batch_size=8,
+        save_steps=1000,
+        save_total_limit=2,
     )
 
     trainable_params = []
     trainable_mod_names = []
 
-    #Only cross attention parameters trainable
-    for name,module in model.named_modules():
-        if 'cross' in name:
+    # Only cross attention parameters trainable
+    for name, module in model.named_modules():
+        if "cross" in name:
+            trainable_mod_names.append(name)
             trainable_params.append(module.parameters())
-    
-    trainable_params = (itertools.chain(*trainable_params))
-    print(trainable_params)
+
+    print(trainable_mod_names)
+    trainable_params = itertools.chain(*trainable_params)
     optimizer = Adam(trainable_params, lr=1e-3)
 
-    total_steps = len(list(tokenized_datasets)) * training_args.num_train_epochs / 8
-    lr_scheduler = get_scheduler(name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=total_steps)
+    total_steps = (
+        len(list(tokenized_datasets)) * training_args.num_train_epochs / args.batch_size
+    )
+    lr_scheduler = get_scheduler(
+        name="linear",
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=total_steps,
+    )
 
     trainer = Trainer(
-    model,
-    training_args,
-    train_dataset=val_dataset,
-    eval_dataset=val_dataset,
-    tokenizer=model_tokenizer,
+        model,
+        training_args,
+        train_dataset=sample_dataset,
+        eval_dataset=sample_dataset,
+        tokenizer=model_tokenizer,
     )
 
     trainer.train()
-    
